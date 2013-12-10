9a495e155a7ec8add16d25a369d9c45c72084562
PETER ATTHEM <peter@PETERs-MacBook-Pro.local>
2013-12-10 18:46:15 +0100

    Uppdaterade UI controller…

--- SQLiteTutorial/ViewController.h
+++ SQLiteTutorial/ViewController.h
@@ -7,8 +7,19 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "sqlite3.h"
 
-@interface ViewController : UIViewController
-//eheh
+@interface ViewController : UIViewController{
+    sqlite3 *contactDB;
+    UITextField     *name;
+    UITextField *address;
+    UITextField *phone;
+    UILabel *status;
+    NSString        *databasePath;
+
+}//hhj
+
+- (IBAction) saveData;
+- (IBAction) findContact;
 
 @end
